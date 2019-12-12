cb176a34e030395c1f5465276063e628fbbe0312
fjl <fjl@mbp>
2019-12-12 15:00:19 +0800

    ga

--- wp/wp-content/themes/basic/header.php
+++ wp/wp-content/themes/basic/header.php
@@ -7,6 +7,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<?php wp_head(); ?>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154412913-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());  gtag('config', 'UA-154412913-1');
+	</script>
 
 </head>
 <body <?php body_class(); ?>>
